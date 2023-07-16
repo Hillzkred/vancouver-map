59a08ab26a0f41a191735575868fa7cf18f14cb3
Alexis Almacin <17034756+Hillzkred@users.noreply.github.com>
2023-07-16 11:10:14 -0700

    Adding map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
+import { DeckGL } from '@deck.gl/react/typed';
+import { Map } from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const MAP_STYLE =
+    'https://api.maptiler.com/maps/basic-v2/style.json?key=ZDFWcNAeAKwpseiIpuuj';
+  const INITIAL_VIEW_STATE = {
+    longitude: -123.1207,
+    latitude: 49.2827,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0,
+  };
+  return (
+    <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
+      <Map mapLib={maplibregl} mapStyle={MAP_STYLE} />
+    </DeckGL>
+  );
 }
 
 export default App;
